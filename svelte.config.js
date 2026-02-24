12740ecbd9761ba9465a90e9e66a9b32faf7feae
Brent Garrison <brent.garrison@wnco.com>
2026-02-23 21:54:37 -0600

    Update base path for parts-inventory repo name

--- svelte.config.js
+++ svelte.config.js
@@ -11,7 +11,7 @@ const config = {
 			strict: true
 		}),
 		paths: {
-			base: process.env.NODE_ENV === 'production' ? '/honda-parts-inventory' : ''
+			base: process.env.NODE_ENV === 'production' ? '/parts-inventory' : ''
 		}
 	}
 };
